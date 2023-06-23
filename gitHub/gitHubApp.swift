ad79678bf00a0e17072ac84a8778e0a1aee009c9
Alexis Bowen <alexisv.bowen@gmail.com>
2023-06-23 10:20:01 -0400

    Initial Commit

---
+++ gitHub/gitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitHubApp.swift
+//  gitHub
+//
+//  Created by Scholar on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct gitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
